12e623e25a034cf00f56c6679986cece78db6e06
leehyeonuk <hwlee@beintech.co.kr>
2023-11-17 16:35:49 +0900

    RedisTemplate list and hash

--- src/main/java/com/example/redisExample/controller/RedisController.java
+++ src/main/java/com/example/redisExample/controller/RedisController.java
@@ -3,11 +3,15 @@ package com.example.redisExample.controller;
 import com.example.redisExample.entity.Member;
 import com.example.redisExample.service.RedisMemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -51,5 +55,55 @@ public class RedisController {
     public void templateTest(){
         ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
         valueOperations.set("testKey","testValue");
+    }
+
+
+    @GetMapping("/templateListTest")
+    public void templateListTest(){
+        ListOperations<String, Object> stringObjectListOperations = redisTemplate.opsForList();
+
+        stringObjectListOperations.rightPush("listTest","h");
+        stringObjectListOperations.rightPush("listTest","e");
+        stringObjectListOperations.rightPush("listTest","l");
+        stringObjectListOperations.rightPush("listTest","l");
+        stringObjectListOperations.rightPush("listTest","o");
+
+        stringObjectListOperations.rightPushAll("listTest"," ","w","o","r","l","d");
+
+        String character_1 = (String) stringObjectListOperations.index("listTest", 1);
+
+        System.out.println("character_1 = " + character_1);
+
+        Long size = stringObjectListOperations.size("listTest");
+
+        System.out.println("size = " + size);
+
+        List<Object> ResultRange = stringObjectListOperations.range("listTest", 0, 10);
+
+        System.out.println("ResultRange = " + Arrays.toString(ResultRange.toArray()));
+
+    }
+
+    @GetMapping("/templateHashTest")
+    public void templateHashTest(){
+        String key = "testHash";
+
+        HashOperations<String, Object, Object> stringObjectObjectHashOperations = redisTemplate.opsForHash();
+
+        stringObjectObjectHashOperations.put(key, "Hello", "helloVal");
+        stringObjectObjectHashOperations.put(key, "Hello2", "helloVal2");
+        stringObjectObjectHashOperations.put(key, "Hello3", "helloVal3");
+
+        Object hello = stringObjectObjectHashOperations.get(key, "Hello");
+
+        System.out.println("hello = " + hello);
+
+        Map<Object, Object> entries = stringObjectObjectHashOperations.entries(key);
+
+        System.out.println("entries = " + entries.get("Hello2"));
+
+        Long size = stringObjectObjectHashOperations.size(key);
+
+        System.out.println("size = " + size);
     }
 }
